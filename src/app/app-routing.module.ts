54c19fcf059729614537ecb46ebdc802ceebabdc
CAPSALC <lunacortesantonio@gmail.com>
2024-12-04 19:28:38 -0600

    Subir proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,44 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'home', // Ruta predeterminada
     pathMatch: 'full'
   },
+  {
+    path: 'acerca',
+    loadChildren: () => import('./pages/acerca/acerca.module').then(m => m.AcercaPageModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./pages/cart/cart.module').then(m => m.CartPageModule)
+  },
+  {
+    path: 'details/:id', // Ruta actualizada con el parámetro de id
+    loadChildren: () => import('./pages/details/details.module').then(m => m.DetailsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule)
+  },
+  {
+    path: 'search-books',
+    loadChildren: () => import('./pages/search-books/search-books.module').then(m => m.SearchBooksPageModule)
+  }
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
-  ],
+  imports: [RouterModule.forRoot(routes, {useHash:true})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
